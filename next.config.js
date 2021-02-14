e6056426808038cf4948032ce40cec9baaaa5096
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-02-14 12:57:43 +0100

    test

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 console.log("CONFIG🔑🔑🔑", apiKey);
 
-module.exports = withCSS({
+module.exports = {
   webpack: (config) => {
     const env = { API_KEY: apiKey };
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   },
-});
+};
